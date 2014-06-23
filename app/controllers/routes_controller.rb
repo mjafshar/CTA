4ff3db5453c49bf234a9cfd92bb47a5142c549cd
Javad Afshar <mjafshar@gmail.com>
2014-06-22 20:57:35 -0500

    Added view to show all routes sorted by the integer in the route number

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,4 +1,5 @@
 class RoutesController < ApplicationController
   def index
+    @routes = Route.all.sort { |a, b| a.route_number.scan(/\d+/).first.to_i <=> b.route_number.scan(/\d+/).first.to_i }
   end
 end
